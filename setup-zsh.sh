7bbd528ed5a431e6174bc9b84ba9f75db37ccd55
kevin-ar-cmd <otienokevino1000@gmail.com>
2025-04-05 14:05:44 +0300

    Add setup-zsh.sh for environment setup

---
+++ setup-zsh.sh
@@ -0,0 +1,64 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Installing Oh My Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Installing Powerlevel10k..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+$HOME/.oh-my-zsh/custom/themes/powerlevel10k
+
+echo "Installing plugins..."
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting \
+$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+echo "Setting up .zshrc..."
+
+cat > ~/.zshrc << 'EOF'
+export ZSH="$HOME/.oh-my-zsh"
+ZSH_THEME="powerlevel10k/powerlevel10k"
+plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+source $ZSH/oh-my-zsh.sh
+
+# Aliases
+alias ll='ls -la --color=auto'
+alias ls='ls --color=auto'
+alias cls='clear'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias g='git'
+alias gs='git status'
+alias ga='git add .'
+alias gc='git commit -m'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gp='git push'
+alias gpull='git pull origin $(git rev-parse --abbrev-ref HEAD)'
+alias gpush='git push origin $(git rev-parse --abbrev-ref HEAD)'
+alias nr='npm run'
+alias ni='npm install'
+alias ns='npm start'
+alias uikit='cd ~/MyMessageToolkit'
+alias update='pkg update && pkg upgrade -y'
+
+# Extras
+HISTFILE=~/.zsh_history
+HISTSIZE=2000
+SAVEHIST=2000
+export PATH="$HOME/bin:$PATH"
+
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+# Welcome
+echo -e "\033[1;32mWelcome back, kevin-ar-cmd! Ready to code like a boss.\033[0m"
+
+if command -v neofetch >/dev/null; then
+  neofetch
+fi
+EOF
+
+echo "Done! Now launch zsh:"
+echo ""
+echo "    zsh"
