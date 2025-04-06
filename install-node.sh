f1ecea5538b2eccaa085c8fe1d71ac229bfa5f7a
Kevin Otieno <182612058+kevin-ar-cmd@users.noreply.github.com>
2025-04-06 06:59:07 +0300

    Delete install-node.sh

--- install-node.sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-set -e
-
-echo "==> Installing Node.js and npm..."
-pkg install -y nodejs
-
-echo "==> Installing global npm tools..."
-npm install -g npm nodemon serve
