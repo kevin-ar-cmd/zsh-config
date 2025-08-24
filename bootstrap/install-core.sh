769fa5bb7a04d286db508fb626975047920ba057
kevin-ar-cmd <otienokevino1000090@gmail.com>
2025-08-24 08:11:20 +0300

    Force keep local changes

--- bootstrap/install-core.sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-set -e
-
-echo "==> Updating packages..."
-pkg update && pkg upgrade -y
-
-echo "==> Installing core packages..."
-pkg install -y git curl wget zsh neofetch
--- bootstrap/install-dev-tools.sh
+++
@@ -1,5 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-set -e
-
-echo "==> Installing dev tools..."
-pkg install -y python python-pip htop fzf
